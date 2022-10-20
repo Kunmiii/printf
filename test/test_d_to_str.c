43497a824371942d8fab264557a1511e475ce290
Oluwatimileyin <oke_joast@protonmail.com>
2022-10-20 08:30:20 -0700

    test dir added

---
+++ test/test_d_to_str.c
@@ -0,0 +1,23 @@
+#include <limits.h>
+#include <stdio.h>
+#include "../main.h"
+
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+
+int main(void)
+{
+
+	double d = 324.23;
+
+	char *s;
+
+	char *f;
+
+	_printf("%f\n", d);
+
+	return (0);
+
+}
